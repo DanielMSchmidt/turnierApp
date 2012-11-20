45841a3787609409d2d4992c1df15d28bf9d9304
Daniel Schmidt <dschmidt@weluse.de>
2012-11-20 11:16:41 +0100

    added tags and added tournament function with_tag

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,9 @@
 class Tournament < ActiveRecord::Base
   attr_accessible :number, :participants, :place, :user_id
+  belongs_to :user
+  def with_tag(tagname)
+    tournaments = []
+    User.tagged_with(tagname).each{ |user| tournaments += user.tournaments}
+    tournaments.sort_by {|a| a[:created_at]} #later should be a thing like :date (after grabbing)
+  end
 end
