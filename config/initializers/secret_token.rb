88f7c73c9ebf77391b239012442812add0f4344d
Daniel Schmidt <dschmidt@weluse.de>
2012-11-20 10:09:40 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurnierList::Application.config.secret_token = '22fdcda1d63c107f70184603363198f8aa97c91c77126b2ab7a6d6c5e3db77e0e236ac9df3bdd6c347c3cbc03d2c17312f1f709b365018d479f9b0f2947e8d0b'
