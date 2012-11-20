6be7eda16f198ffd49ca0e3fb9b3acc9823f13f6
Daniel Schmidt <dschmidt@weluse.de>
2012-11-20 10:20:41 +0100

    installed devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ TurnierList::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Host_url set
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TurnierList::Application.routes.draw do
+
+  root :to => "home#index"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
