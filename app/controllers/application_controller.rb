26028f6471cb3d5bc5892280db5fa4378364ed0b
Daniel Schmidt <dschmidt@weluse.de>
2012-11-29 17:32:32 +0100

    fixed bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
   before_filter :get_clubs
 
   def get_clubs
-    @own_clubs = current_user.clubs
-    @clubs = Club.all.delete_if {|club| @own_clubs.include?(club) }
+    if user_signed_in?
+      @own_clubs = current_user.clubs
+      @clubs = Club.all.delete_if {|club| @own_clubs.include?(club) }
+    else
+      @own_clubs = nil
+      @clubs = Club.all
+    end
   end
 end
