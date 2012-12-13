a08c2a80fc888c7535c43691dec9752af9370beb
Daniel Schmidt <dschmidt@weluse.de>
2012-12-13 22:11:25 +0100

    deleted GA

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,16 +14,3 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
-
-_gaq = _gaq or []
-_gaq.push ["_setAccount", "UA-37020620-1"]
-_gaq.push ["_setDomainName", "herokuapp.com"]
-_gaq.push ["_trackPageview"]
-(->
-  ga = document.createElement("script")
-  ga.type = "text/javascript"
-  ga.async = true
-  ga.src = ((if "https:" is document.location.protocol then "https://ssl" else "http://www")) + ".google-analytics.com/ga.js"
-  s = document.getElementsByTagName("script")[0]
-  s.parentNode.insertBefore ga, s
-)()
