3db6833b6cccccc0908adc830a06150280822536
Daniel Schmidt <dschmidt@weluse.de>
2012-12-27 15:16:57 +0100

    locked pwd recovery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ TurnierList::Application.configure do
 
   # Mailer
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 25, :user_name => ENV['PWD_MAILER_NAME'], :password => ENV['PWD_MAILER_PWD'] }
+  config.action_mailer.smtp_settings = { :address => "smtp.1und1.de", :port => 25, :user_name => ENV['PWD_MAILER_NAME'], :password => ENV['PWD_MAILER_PWD'] }
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.raise_delivery_errors = true
 
