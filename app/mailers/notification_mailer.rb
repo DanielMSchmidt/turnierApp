0e908eb43f33332f7b9544d4f4f56b2769a29061
Daniel Schmidt <dschmidt@weluse.de>
2012-12-29 18:12:01 +0100

    added function to mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,3 +1,14 @@
 class NotificationMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notification_mailer.enrollCouples.subject
+  #
+  def enrollCouples
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
 end
