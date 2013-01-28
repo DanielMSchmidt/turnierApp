56c03826bedcde683adea78fdaf2c9c3ea7c4dd0
Daniel Schmidt <dschmidt@weluse.de>
2013-01-28 12:58:23 +0100

    translated notice

--- app/controllers/membership_controller.rb
+++ app/controllers/membership_controller.rb
@@ -1,11 +1,11 @@
 class MembershipController < ApplicationController
   def create
-    Membership.create!(user_id: params[:user_id], club_id: params[:club_id])
-    redirect_to edit_user_registration_path(current_user), notice: 'Club was successfully added.'
+    Membership.create!(user_id: params[:user_id], club_id: params[:club_id], verified: false)
+    redirect_to edit_user_registration_path(current_user), notice: 'Der Verein wurde erfolgreich zu deinen Vereinen hinzugefuegt. Sie muessen jetzt lediglich noch bestaetigt werden.'
   end
 
   def destroy
     @membership = Membership.destroy_all(club_id: params[:club_id], user_id: params[:user_id])
-    redirect_to edit_user_registration_path(current_user), notice: 'Club was successfully deleted.'
+    redirect_to edit_user_registration_path(current_user), notice: 'Der Verein wurde erfolgreich aus deinen Vereinen geloescht.'
   end
 end
