82eda8ef81af10a5e4f547a3dbe883afa92422f8
Daniel Schmidt <dschmidt@weluse.de>
2013-01-28 12:59:27 +0100

    added verified column for memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,8 @@
 class Membership < ActiveRecord::Base
-  attr_accessible :club_id, :user_id
+  attr_accessible :club_id, :user_id, :verified
   belongs_to :user
   belongs_to :club
+
+  scope :verified, where(verified: true)
+  scope :unverified, where(verified: false)
 end
