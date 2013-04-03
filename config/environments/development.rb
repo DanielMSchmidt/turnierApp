f7be49ae454fa1a41764cf1861a66a2794f249fc
Daniel Schmidt <dschmidt@weluse.de>
2013-04-03 23:15:02 +0200

    fixed bullet bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,12 +43,11 @@ TurnierList::Application.configure do
 
 
   #Bullet gem configurations
-    config.after_initialize do
+  config.after_initialize do
     Bullet.enable = true
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.rails_logger = true
-    Bullet.disable_browser_cache = true
   end
 end
