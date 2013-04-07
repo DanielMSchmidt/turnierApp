9d270d6eeea00b9c78484867e804da4ac67f8779
Daniel Schmidt <dschmidt@weluse.de>
2013-04-07 13:32:02 +0200

    added modal for adding a club to a user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,21 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      checkIfUserIsReadyToStart(current_user)
+      setUserModalData
+    end
   end
 
   def impressum
     @active_page = 'impressum'
+  end
+
+  def checkIfUserIsReadyToStart(user)
+    @has_couple = user.activeCouple.present?
+    @has_a_club = user.clubs.first.present?
+  end
+
+  def setUserModalData
+    @clubs = Club.all
   end
 end
