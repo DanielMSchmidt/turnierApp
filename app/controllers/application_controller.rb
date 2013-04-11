4ea83c09fdd3093aee175a611a0a5d9c66c49ad7
Daniel Schmidt <dschmidt@weluse.de>
2013-04-11 11:32:23 +0200

    set modal data everywere if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :setUserModalDataIfLoggedIn
+
   def after_sign_in_path_for(resource)
     root_path
+  end
+
+  def setUserModalDataIfLoggedIn
+    setUserModalData if user_signed_in?
+  end
+
+  def setUserModalData
+    @clubs = Club.all
+    @couple = Couple.new
+    @tournament = Tournament.new
   end
 end
