02c7f70b225e21213cb6d6c5d01d5fe991422e02
Daniel Schmidt <dschmidt@weluse.de>
2013-07-12 08:35:00 +0200

    deleted unused function

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,15 +1,10 @@
 # -*- encoding : utf-8 -*-
 class Membership < ActiveRecord::Base
   attr_accessible :club_id, :couple_id, :verified
-  belongs_to :coulpe
-  belongs_to :club
+  belongs_to :coulpe, :foreign_key =>:coulpe_id
+  belongs_to :club, :foreign_key =>:club_id
 
-  default_scope includes(:user)
   scope :is_verified, where(verified: true)
   scope :is_unverified, where(verified: false)
 
-
-  def user
-    self.couple.users
-  end
 end
