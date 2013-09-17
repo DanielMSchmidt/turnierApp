c1af22a02944a504eb2cd8410186374b684a6ead
Daniel Schmidt <dschmidt@weluse.de>
2013-09-17 20:31:14 +0200

    fixed precompilation bug

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,7 @@ module TurnierList
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['pdf.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
