a26f752c3d3cc0e5e4515e61ace9b5a7a10551ff
Daniel Schmidt <dschmidt@weluse.de>
2013-11-19 15:30:44 +0100

    added retry support

--- app/workers/tournament_enhancement_worker.rb
+++ app/workers/tournament_enhancement_worker.rb
@@ -1,13 +1,19 @@
 class TournamentEnhancementWorker
   include Sidekiq::Worker
+  sidekiq_options :retry => 5
 
   def perform(tournament_id, tournament_number, user_id)
     logger.debug { "Worker starts for (#{tournament_id} / #{tournament_number} / #{user_id})" }
     tournament_infos = TournamentFetcher.new(tournament_number).run
     tournament = Tournament.find(tournament_id)
 
+    if [:kind, :address, :date].map{|x| tournament_infos[x].blank? }.include?(true)
+      logger.debug "Worker abords"
+      raise "There was something blank fetched"
+      return false
+    end
 
-    [:kind, :address, :date, :kind, :notes].each do |key|
+    [:kind, :address, :date, :notes].each do |key|
       tournament[key] = tournament_infos[key]
     end
 
